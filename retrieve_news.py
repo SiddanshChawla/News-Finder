23dd74580c04280e3ec6559727669d47be0a2bde
Siddansh Chawla <71915630+SiddanshChawla@users.noreply.github.com>
2023-04-20 18:28:25 +0530

    Update retrieve_news.py

--- retrieve_news.py
+++ retrieve_news.py
@@ -30,5 +30,5 @@ for keyword in important_keywords:
 for article in articles:
     st.write(article['title'])
     st.write(article['description'])
-    st.write(article['url']
+    st.write(article['url'])
     st.write('---')
