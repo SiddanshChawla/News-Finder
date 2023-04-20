bc69c450a45eb8dc739030b485b6a04b1fa68173
Siddansh Chawla <71915630+SiddanshChawla@users.noreply.github.com>
2023-04-20 18:13:16 +0530

    Add files via upload

---
+++ retrieve_news.py
@@ -0,0 +1,31 @@
+from newsapi import NewsApiClient
+from news import extract_phrases
+import streamlit as st
+import numpy as np
+from datetime import date
+from datetime import timedelta
+
+today = date.today()
+week_before = today - timedelta(days = 7)
+st.write(week_before)
+
+# Set up NewsAPI client
+newsapi = NewsApiClient(api_key='0eab831ebf9c402ba6f4f2312b355ad6')
+
+# Example description
+description = st.text_input("Enter the text to search news article...")
+keywords = extract_phrases(description)
+important_keywords = keywords[:2]
+
+# Search for news articles related to the keywords
+articles = []
+for keyword in important_keywords:
+    keyword = keyword
+    news = newsapi.get_everything(q=keyword, from_param=week_before, language='en')
+    articles.extend(news['articles'])
+
+# Print article titles and description
+for article in articles:
+    st.write(article['title'])
+    st.write(article['description'])
+    st.write('---')
